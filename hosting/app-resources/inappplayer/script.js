045a073991dfb254029959b193e72d58fb6f09be
Zach Buchar - Envy <zach@zachnology.com>
2023-12-27 11:30:46 -0600

    Merge existing firebase configuration

---
+++ hosting/app-resources/inappplayer/script.js
@@ -0,0 +1,1 @@
+var myaudio = new Audio('https://anchor.fm/s/28075148/podcast/play/15649722/sponsor/a3vggso/https%3A%2F%2Fd3ctxlq1ktw2nl.cloudfront.net%2Fstaging%2F2021-08-17%2Fde25f986f5dac0a41bed455ca9cb0390.m4a');
